fd25e6b8ea559663253e207bd6f326b9e86b4134
Meqdad Amr <Meqdad_@outlook.com>
2023-07-18 10:46:01 +0300

    13.12

--- src/app.js
+++ src/app.js
@@ -2,10 +2,15 @@ const express = require('express')
 const app = express()
 require('express-async-errors')
 
-const blogsRouter = require('./controllers/blogs')
+const notesRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
+
 app.use(express.json())
 
-app.use('/api/blogs/', blogsRouter)
+app.use('/api/blogs', notesRouter)
+app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use('/', (req, res) => {
   res.send('Hello World!')
